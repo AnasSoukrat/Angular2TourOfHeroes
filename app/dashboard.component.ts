d10bc62bb1cfec1bc992600c0733e006c4ecef7a
AnasSoukrat <Anassoukrat2013@gmail.com>
2016-11-27 12:50:22 +0000

    Http

--- app/dashboard.component.ts
+++ app/dashboard.component.ts
@@ -23,6 +23,7 @@ export class DashboardComponent implements OnInit {
     ngOnInit(): void {
         this.heroService.getHeroes()
             .then(heroes => this.heroes = heroes.slice(1, 5));
+            // .then(heroes =>console.log(heroes));
     }
 }
 
